b12001fee59e356b720c76a110c80062a1a573c5
Lucas Morais <lucascdemorais@gmail.com>
2024-11-12 17:13:14 -0300

    adjusting colors to darkest

--- lua/dracula/palette.lua
+++ lua/dracula/palette.lua
@@ -1,55 +1,56 @@
 -- ---@class Palette
 -- return {
---    bg = "#282A36",
---    fg = "#F8F8F2",
---    selection = "#44475A",
---    comment = "#6272A4",
---    red = "#FF5555",
---    orange = "#FFB86C",
---    yellow = "#F1FA8C",
---    green = "#50fa7b",
---    purple = "#BD93F9",
---    cyan = "#8BE9FD",
---    pink = "#FF79C6",
---    bright_red = "#FF6E6E",
---    bright_green = "#69FF94",
---    bright_yellow = "#FFFFA5",
---    bright_blue = "#D6ACFF",
---    bright_magenta = "#FF92DF",
---    bright_cyan = "#A4FFFF",
---    bright_white = "#FFFFFF",
---    menu = "#21222C",
---    visual = "#3E4452",
---    gutter_fg = "#4B5263",
---    nontext = "#3B4048",
---    white = "#ABB2BF",
---    black = "#191A21",
+--    bg = "#1E1F29",              -- Darker background color
+--    fg = "#E0E0E0",              -- Slightly darker foreground color
+--    selection = "#333444",       -- Darker selection background
+--    comment = "#51577A",         -- Darker comment color
+--    red = "#D84050",             -- Darker red
+--    orange = "#D96B46",          -- Darker orange
+--    yellow = "#E6E24C",          -- Darker yellow
+--    green = "#41D880",           -- Darker green
+--    purple = "#9E7BDA",          -- Darker purple
+--    cyan = "#5FCFE6",            -- Darker cyan
+--    pink = "#D465A6",            -- Darker pink
+--    bright_red = "#E84A4A",      -- Darker bright red
+--    bright_green = "#5FE39E",    -- Darker bright green
+--    bright_yellow = "#E7E574",   -- Darker bright yellow
+--    bright_blue = "#9B82F9",     -- Darker bright blue
+--    bright_magenta = "#E76EC6",  -- Darker bright magenta
+--    bright_cyan = "#80E6F7",     -- Darker bright cyan
+--    bright_white = "#EDEDED",    -- Darker bright white
+--    menu = "#1B1C25",            -- Darker menu background
+--    visual = "#2B2D3B",          -- Darker visual mode selection
+--    gutter_fg = "#3C3F50",       -- Darker gutter foreground
+--    nontext = "#2A2C37",         -- Darker non-text characters
+--    white = "#959DAA",           -- Darker white text
+--    black = "#101015",           -- Even darker black
 -- }
---
+
 ---@class Palette
 return {
-   bg = "#1E1F29",              -- Darker background color
-   fg = "#E0E0E0",              -- Slightly darker foreground color
-   selection = "#333444",       -- Darker selection background
-   comment = "#51577A",         -- Darker comment color
-   red = "#D84050",             -- Darker red
-   orange = "#D96B46",          -- Darker orange
-   yellow = "#E6E24C",          -- Darker yellow
-   green = "#41D880",           -- Darker green
-   purple = "#9E7BDA",          -- Darker purple
-   cyan = "#5FCFE6",            -- Darker cyan
-   pink = "#D465A6",            -- Darker pink
-   bright_red = "#E84A4A",      -- Darker bright red
-   bright_green = "#5FE39E",    -- Darker bright green
-   bright_yellow = "#E7E574",   -- Darker bright yellow
-   bright_blue = "#9B82F9",     -- Darker bright blue
-   bright_magenta = "#E76EC6",  -- Darker bright magenta
-   bright_cyan = "#80E6F7",     -- Darker bright cyan
-   bright_white = "#EDEDED",    -- Darker bright white
-   menu = "#1B1C25",            -- Darker menu background
-   visual = "#2B2D3B",          -- Darker visual mode selection
-   gutter_fg = "#3C3F50",       -- Darker gutter foreground
-   nontext = "#2A2C37",         -- Darker non-text characters
-   white = "#959DAA",           -- Darker white text
-   black = "#101015",           -- Even darker black
+   bg = "#0E1419",              -- Background color
+   fg = "#F8F8F2",              -- Foreground color
+   selection = "#44475A",       -- Selection background
+   comment = "#6272A4",         -- Comment color
+   red = "#FF5555",             -- Red
+   orange = "#FFB86C",          -- Orange
+   yellow = "#F1FA8C",          -- Yellow
+   green = "#50FA7B",           -- Green
+   purple = "#BD93F9",          -- Purple
+   cyan = "#8BE9FD",            -- Cyan
+   pink = "#FF79C6",            -- Pink
+   bright_red = "#DE312B",      -- Bright red
+   bright_green = "#2FD651",    -- Bright green
+   bright_yellow = "#D0D662",   -- Bright yellow
+   bright_blue = "#9C6FCF",     -- Bright blue
+   bright_magenta = "#DE559C",  -- Bright magenta
+   bright_cyan = "#6AC5D3",     -- Bright cyan
+   bright_white = "#F8F8F2",    -- Bright white
+   menu = "#3A434D",            -- Menu background
+   visual = "#253340",          -- Visual mode selection
+   gutter_fg = "#44475A",       -- Gutter foreground
+   nontext = "#424450",         -- Non-text characters
+   white = "#FFFFFF",           -- White text
+   black = "#191A21",           -- Black
 }
+
